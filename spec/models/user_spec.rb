c9668ff4a0362305c195197381c6c2fa0565f96a
Kosuke Ito <keree.it.owl.uk@gmail.com>
2021-07-10 15:56:44 +0900

    ユーザーのフルネームを文字列で返すテストの作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,25 +25,34 @@ RSpec.describe User, type: :model do
   end
   # メールアドレスがなければ無効な状態であること
   it "is invalid without an email address" do
+    user = User.new(email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
+  end
+  # 重複したメールアドレスなら無効な状態であること
+  it "is invalid with a duplicate email address" do
     User.create(
       first_name: "Joe",
       last_name: "Tester",
       email: "tester@example.com",
-      password: "dottle-nouveau-pavilion-tights-furze"
+      password: "dottle-nouveau-pavilion-tights-furze",
     )
     user = User.new(
-      first_name:  "Jane",
-      last_name:  "Tester",
-      email:      "tester@example.com",
-      password:   "dottle-nouveau-pavilion-tights-furze",
+    first_name: "Jane",
+    last_name: "Tester",
+    email: "tester@example.com",
+    password: "dottle-nouveau-pavilion-tights-furze",
     )
     user.valid?
     expect(user.errors[:email]).to include("has already been taken")
   end
-  # 重複したメールアドレスなら無効な状態であること
-  it "is invalid with a duplicate email address" do
-    
-  end
   # ユーザーのフルネームを文字列として返すこと
-  it "returns a user's full name as a string"
+  it "returns a user's full name as a string" do
+    user = User.new(
+      first_name: "John",
+      last_name: "Doe",
+      email: "johndoe@example.com",
+    )
+    expect(user.name).to eq "John Doe"
+  end
 end
