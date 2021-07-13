c2958b0e5b37d74f527ce904cba1d54389bbfd6e
Kosuke Ito <keree.it.owl.uk@gmail.com>
2021-07-13 13:14:40 +0900

    You cannot call create unless the parent is savedのエラー原因修正

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe Project, type: :model do
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it 'does not allow duplicate project names per user' do
     user = User.create(
-      first_name: "Joe",
+      first_name: "Jo",
       last_name: "Tester",
-      email: "joetester@example.com",
+      email: "jotester@example.com",
       password: "dottle-nouveau-pavilion-tights-furze"
     )
 
-    user.projects.create(
+  user.projects.create(
       name: "Test Project"
     )
 
@@ -25,10 +25,14 @@ RSpec.describe Project, type: :model do
   # 二人のユーザーが同じ名前を使うことは許可すること
   it "allows two users to share a project name" do
     user = User.create(
-      first_name: "Joe",
+      first_name: "Joen",
       last_name: "Tester",
-      email: "joetester@example.com",
+      email: "joentester@example.com",
       password: "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    user.projects.create(
+      name: "Test Project"
     )
 
     other_user = User.create(
@@ -37,6 +41,7 @@ RSpec.describe Project, type: :model do
       email: "janetester@example.com",
       password: "dottle-nouveau-pavilion-tights-furze",
     )
+
     other_project = other_user.projects.build(
       name: "Test Project",
     )
