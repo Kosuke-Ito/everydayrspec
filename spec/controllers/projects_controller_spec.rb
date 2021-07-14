121fd22f3cbf7933cbe131670e81e14fcf06779d
Kosuke Ito <keree.it.owl.uk@gmail.com>
2021-07-14 18:19:31 +0900

    プロジェクトコントローラー#show追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe ProjectsController, type: :controller do
 
   describe "#index" do
-
     # 認証済みのユーザーとして
     context "as an authenticated user" do
       before do
@@ -39,7 +38,38 @@ RSpec.describe ProjectsController, type: :controller do
         expect(response).to redirect_to "/users/sign_in"
       end
     end
-
   end
 
+  describe "#show" do
+    # 認可されたユーザとして
+    context "as an authorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      # 正常にレスポンスを返すこと
+      it "responds successfully" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to be_success 
+      end
+    end
+
+    # 認可されていないユーザーとして
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryBot.create(:user)
+        other_user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: other_user)
+      end
+
+      # ダッシュボードにリダイレクトすること
+      it "redirects to the dashboard" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
 end
