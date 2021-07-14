ec1b268c6f66336b889ce2f496dfdf80ca3dce23
Kosuke Ito <keree.it.owl.uk@gmail.com>
2021-07-14 17:45:18 +0900

    traitでリファクタリング、コールバックの追加

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
+
   # ユーザー単位では重複したプロジェクト名を許可しないこと
   it 'does not allow duplicate project names per user' do
     user = User.create(
@@ -47,4 +48,34 @@ RSpec.describe Project, type: :model do
     )
     expect(other_project).to be_valid
   end
+
+  describe "late status" do
+
+    # 締切日が過ぎていれば遅延していること
+    it "is late when the due date is past today" do
+      project = FactoryBot.create(:project, :due_yesterday)
+      expect(project).to be_late
+    end
+
+    # 締切日が今日ならスケジュールどおりであること
+    it "is on time when the due date is today" do
+      project = FactoryBot.create(:project, :due_today)
+      expect(project).to_not be_late
+    end
+
+    # 締切日が未来ならスケジュールどおりであること
+    it "is on time when the due date is in the future" do
+      project = FactoryBot.create(:project, :due_tomorrow)
+      expect(project).to_not be_late
+    end
+
+  end
+
+
+  # たくさんのメモが付いていること
+  it "can have many notes" do
+    project = FactoryBot.create(:project, :with_notes)
+    expect(project.notes.length).to eq 5
+  end
+
 end
