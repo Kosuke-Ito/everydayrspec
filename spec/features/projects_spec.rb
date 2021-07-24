f764c6ab1a1d96116c2dcbfa0160fe0e1eb072c6
Kosuke Ito <keree.it.owl.uk@gmail.com>
2021-07-24 23:26:28 +0900

    loginサポートモジュール追加

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -4,23 +4,17 @@ RSpec.feature "Projects", type: :feature do
   # ユーザーは新しいプロジェクトを作成する
   scenario "user creates anew project" do
     user = FactoryBot.create(:user)
-
-    visit root_path
-    click_link "Sign in"
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
+    sign_in_as user
 
     expect{ # capybara DSL
       click_link "New Project"
       fill_in "Name",	with: "Test Project" 
       fill_in "Description",	with: "Trying out Capybara"
       click_button "Create Project"
-
-      expect(page).to have_content "Project was successfully created"
-      expect(page).to have_content "Test Project"
-      expect(page).to have_content "Owner: #{user.name}"
     }.to change(user.projects, :count).by(1)
-  end
 
+    expect(page).to have_content "Project was successfully created"
+    expect(page).to have_content "Test Project"
+    expect(page).to have_content "Owner: #{user.name}"
+  end
 end
